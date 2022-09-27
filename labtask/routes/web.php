fb22cf43943668404ce3c1d965c1d6548211e19b
Protik Acharjay <protik7777777@gmail.com>
2022-09-27 22:59:05 +0600

    web php code copied from sir

--- labtask/routes/web.php
+++ labtask/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', [PagesController::class, 'index'])->name('hello');
+Route::get('/profile',[PagesController::class, 'profile'])->name('profile');
