87b7b277879c932ebf01f0079ed296300f59595f
Protik Acharjay <protik7777777@gmail.com>
2022-10-31 22:07:58 +0600

    log in Migration registration session done

---
+++ APWT_TASK_3/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('/auth/login');
+});
+Route::get('/auth/login',[MainController::class,'login'])->name('auth.login');
+Route::post('/auth/login',[MainController::class,'loggedin'])->name('auth.login');
+Route::get('/auth/register',[MainController::class,'register'])->name('auth.register');
+Route::post('/auth/register',[MainController::class,'registered'])->name('auth.register');
+
+Route::get('/customer/homepage',[MainController::class,'homepage']);
